de4f80a1bce15cb0de47b719e317fdbd0c09327d
Taofeek <taofeekdboy@yahoo.com>
2022-10-08 11:29:46 +0300

    removed build files

--- build/src/utils/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    apiBaseUrl: 'https://reqres.in/api',
-    dataUserProperty: ['id', 'first_name', 'last_name', 'email', 'avatar'],
-    dataResourceProperty: ['id', 'name', 'pantone_value', 'color', 'year'],
-    dataCreationProperty: ['id', 'name', 'job', 'createdAt'],
-    dataProps: { name: 'taofeek', job: 'tester', email: 'eve.holt@reqres.in', password: 'cityslicka' }
-};
